036dedf30daacdbfc6ffb94c6197950c1ddae859
Amal-Vincent <amal.vincent1993@hotmail.com>
2019-03-24 21:30:47 -0700

    Cleaned up code

--- js/background.js
+++ js/background.js
@@ -108,14 +108,6 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
 update = function() {
   stats.begin();
   stats.end();
@@ -124,4 +116,4 @@ update = function() {
   }
   requestAnimationFrame(update);
 };
-requestAnimationFrame(update);;
+requestAnimationFrame(update);
