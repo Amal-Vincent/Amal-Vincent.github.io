b3f08ed007fde0b69b48c69c1c9b5bde2d68c93b
Amal Vincent <packry@bitbucket.org>
2019-10-30 17:53:22 -0700

    Made responsive for largescreen monitors

--- js/main.js
+++ js/main.js
@@ -2,7 +2,31 @@ $('.navbar-nav>li>a').on('click', function() {
   console.log("working");
   $('.navbar-collapse').collapse('hide');
 });
+$(document).ready(() => {
+  let url = location.href.replace(/\/$/, "");
 
+  if (location.hash) {
+    const hash = url.split("#");
+    $('#myTab a[href="#'+hash[1]+'"]').tab("show");
+    url = location.href.replace(/\/#/, "#");
+    history.replaceState(null, null, url);
+    setTimeout(() => {
+      $(window).scrollTop(0);
+    }, 400);
+  }
+
+  $('a[data-toggle="tab"]').on("click", function() {
+    let newUrl;
+    const hash = $(this).attr("href");
+    if(hash == "#home") {
+      newUrl = url.split("#")[0];
+    } else {
+      newUrl = url.split("#")[0] + hash;
+    }
+    newUrl += "/";
+    history.replaceState(null, null, newUrl);
+  });
+});
 $('.lightbox').click(function() {
   var title = $(this).attr('title');
   var src = $(this).children('img').attr("src").replace('/240/140/', '/480/280/');
